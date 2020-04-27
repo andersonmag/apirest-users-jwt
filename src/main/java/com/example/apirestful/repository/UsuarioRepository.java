a95de2811e372fe6b8dd4734f90adbd931912df0
Anderson <adnrepper@gmail.com>
2020-04-27 15:16:22 -0300

    Metodo de busca por email no repositorio

--- src/main/java/com/example/apirestful/repository/UsuarioRepository.java
+++ src/main/java/com/example/apirestful/repository/UsuarioRepository.java
@@ -1,10 +1,10 @@
 package com.example.apirestful.repository;
 
-import javax.transaction.Transactional;
 import com.example.apirestful.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Transactional
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByEmail(String email);
+}
