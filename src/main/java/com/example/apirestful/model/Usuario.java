681f3323479e01f3ca0fc80d0a76d9b0785ee95b
Anderson <adnrepper@gmail.com>
2020-04-27 14:50:43 -0300

    Mudança no projeto, Contact.class < Usuario.class | UserDetails para security, Relacionamento com Role e Notas

--- src/main/java/com/example/apirestful/model/Usuario.java
+++ src/main/java/com/example/apirestful/model/Usuario.java
@@ -1,23 +1,33 @@
 package com.example.apirestful.model;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import io.swagger.annotations.ApiModelProperty;
 
 @Entity
-public class Usuario implements Serializable{
+public class Usuario implements UserDetails {
 
     private static final long serialVersionUID = 1L;
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @ApiModelProperty(value = "Id do Usuario")
     private Long id;
 
@@ -25,16 +35,24 @@ public class Usuario implements Serializable{
     private String nome;
 
     @ApiModelProperty(value = "Email do Usuario")
-    @Nullable
+    @Column(nullable = false, unique = true)
     private String email;
-    
+
     @Nullable
     private String senha;
 
     @ApiModelProperty(value = "Lista de Notas do Usuario")
-    @OneToMany(mappedBy = "usuario", orphanRemoval = true,
-     cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Nota> notas;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "usuarios_role", uniqueConstraints = @UniqueConstraint(
+            columnNames = {"usuario_id", "role_id"}, name = "unique_role_user"),
+        joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false,
+        foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role", unique = false, updatable = false,
+        foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)))
+    private List<Role> roles;
 
     public Long getId() {
         return id;
@@ -74,5 +92,47 @@ public class Usuario implements Serializable{
 
     public void setNotas(List<Nota> notas) {
         this.notas = notas;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
